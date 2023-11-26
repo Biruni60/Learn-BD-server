dcc089e0ecff8a2b86f1463b9f7d5b35dee336c7
biru60 <u1904060@student.cuet.ac.bd>
2023-11-26 21:04:35 +0600

    admin,teacher route setup done,teacher can add update and delete courses

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const port=process.env.PORT ||5000
 
 
   
-  app.use(cors());
+app.use(cors());
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fjhyf9f.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,16 +25,56 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     
 
     const userCollection=client.db("learnDB").collection("users")
+    const classCollection=client.db("learnDB").collection("classes")
 
     //user
+    
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+     
+
+    app.get('/userprofile/:email', async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      res.send(user)
+    })
+
+    
+
+
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+    app.get('/users/teacher/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let teacher = false;
+      if (user) {
+        teacher = user?.role === 'teacher';
+      }
+      res.send({ teacher });
+    })
+
     app.post('/users', async (req, res) => {
         const user = req.body;
-        // insert email if user doesnt exists: 
-        // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
         const query = { email: user.email }
         const existingUser = await userCollection.findOne(query);
         if (existingUser) {
@@ -43,7 +83,61 @@ async function run() {
         const result = await userCollection.insertOne(user);
         res.send(result);
       });
+      app.put('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }   
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
 
+
+    // classes
+    app.post('/classes', async (req, res) => {
+      const item = req.body;
+      const result = await classCollection.insertOne(item);
+      res.send(result);
+    });
+    //Teachers
+    app.get('/classes/:email',async(req,res)=>{
+      const email=req.params.email 
+    
+        const query={email:email}
+        const result=await classCollection.find(query).toArray()
+        res.send(result)
+      
+    })
+    
+    app.put('/classes/:id', async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updatedDoc = {
+        $set: {
+                title:item.title,
+                name:item.name,
+                email:item.email,
+                price:item.price,
+                description:item.description,
+                image:item.image,
+        }
+      }
+
+      const result = await classCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+    })
+   
+
+    app.delete('/classes/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await classCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
