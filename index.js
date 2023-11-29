e2ea810b926ecbaed8cba748c284f657a5de98ae
biru60 <u1904060@student.cuet.ac.bd>
2023-11-29 21:00:22 +0600

    jwt related works done

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express=require("express")
 const cors=require("cors")
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const app =express()
 const port=process.env.PORT ||5000
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-  
-app.use(cors());
+
+const corsOptions = {
+  origin:[
+  'http://localhost:5173'
+  ],
+  credentials:true,
+  optionSuccessStatus: 200
+
+}
+app.use(cors(corsOptions));
 app.use(express.json())
+app.use(cookieParser())
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -35,15 +46,63 @@ async function run() {
     const evaluationCollection=client.db("learnDB").collection("evaluation")
     const submissionCollection=client.db("learnDB").collection("submission")
     const appliedCollection=client.db("learnDB").collection("applied")
+    
+    
+    //jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+    
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+    
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+    const verifyTeacher = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isTeacher = user?.role === 'teacher';
+      if (!isTeacher) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+
+    
     //user
     
-    app.get('/users', async (req, res) => {
+    app.get('/users',verifyToken,verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
      
 
-    app.get('/userprofile/:email', async (req, res) => {
+    app.get('/userprofile/:email',verifyToken, async (req, res) => {
       const email = req.params.email;
       console.log(email);
       const query = { email: email };
@@ -55,9 +114,13 @@ async function run() {
     
 
 
-    app.get('/users/admin/:email', async (req, res) => {
+    app.get('/users/admin/:email',verifyToken, async (req, res) => {
       const email = req.params.email;
-      console.log(email);
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
       const query = { email: email };
       const user = await userCollection.findOne(query);
       let admin = false;
@@ -67,8 +130,11 @@ async function run() {
       res.send({ admin });
     })
 
-    app.get('/users/teacher/:email', async (req, res) => {
+    app.get('/users/teacher/:email',verifyToken, async (req, res) => {
       const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
       const query = { email: email };
       const user = await userCollection.findOne(query);
       let teacher = false;
@@ -88,7 +154,7 @@ async function run() {
         const result = await userCollection.insertOne(user);
         res.send(result);
       });
-      app.put('/users/admin/:id', async (req, res) => {
+      app.put('/users/admin/:id',verifyToken,verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
@@ -117,6 +183,14 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/class-stats/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = {classId:id}
+      const enrollment=await paymentCollection.find(query).toArray()
+      res.send(enrollment)
+    })
+
     app.get("/myclass",async(req,res)=>{
       const email = req.query.email;
       const query = { email: email };
@@ -135,24 +209,24 @@ async function run() {
     })
 
 
-    app.post('/evaluation', async (req, res) => {
+    app.post('/evaluation',verifyToken, async (req, res) => {
       const item = req.body;
       const result = await evaluationCollection.insertOne(item);
       res.send(result);
     });
 
-    app.post('/submission', async (req, res) => {
+    app.post('/submission',verifyToken, async (req, res) => {
       const item = req.body;
       const result = await submissionCollection.insertOne(item);
       res.send(result);
     });
-    app.post('/apply', async (req, res) => {
+    app.post('/apply',verifyToken, async (req, res) => {
       const item = req.body;
       const result = await appliedCollection.insertOne(item);
       res.send(result);
     });
     
-    app.get('/apply/:email',async(req,res)=>{
+    app.get('/apply/:email',verifyToken,async(req,res)=>{
       const email=req.params.email 
     
         const query={email:email}
@@ -172,7 +246,7 @@ async function run() {
    res.send(result);
   }); 
  
-  app.put('/approveclass/:id', async (req, res) => {
+  app.put('/approveclass/:id',verifyToken,verifyAdmin, async (req, res) => {
     const id = req.params.id;
     const data=req.body.item
     console.log(id,data);
@@ -198,7 +272,7 @@ async function run() {
    }); 
   
      //Teachers
-    app.post('/classes', async (req, res) => {
+    app.post('/classes',verifyToken,verifyTeacher, async (req, res) => {
       const item = req.body;
       const result = await classCollection.insertOne(item);
       res.send(result);
@@ -233,14 +307,14 @@ async function run() {
     })
    
 
-    app.delete('/classes/:id', async (req, res) => {
+    app.delete('/classes/:id',verifyToken,verifyTeacher, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await classCollection.deleteOne(query);
       res.send(result);
     })
 
-    app.get('/teacher-stats/:id', async (req, res) => {
+    app.get('/teacher-stats/:id',verifyToken,verifyTeacher, async (req, res) => {
       const id = req.params.id;
       console.log(id);
       const query = {classId:id}
@@ -257,6 +331,7 @@ async function run() {
       res.send({users,classes,enrollments})
     })
 
+
     //teacher request
 
     app.get('/teacherrequest', async (req, res) => {
@@ -264,7 +339,7 @@ async function run() {
       res.send(result);
      }); 
 
-     app.put('/approveteacher/:id', async (req, res) => {
+     app.put('/approveteacher/:id',verifyToken,verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const data=req.body.item
       const filter = { _id: new ObjectId(id) };
@@ -333,7 +408,7 @@ async function run() {
     
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
